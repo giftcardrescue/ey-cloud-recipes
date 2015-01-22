c6904d89b68319b58a0cb5f222c3f309da3ba782
Bill Horsman <bill@logicalcobwebs.com>
2015-01-22 10:43:31 +0000

    Add staging and production logs to LE

--- cookbooks/le/recipes/configure.rb
+++ cookbooks/le/recipes/configure.rb
@@ -6,13 +6,15 @@
 execute "le register --account-key" do
   command "le register --account-key #{node[:le_api_key]} --name #{node[:applications].keys.first}"
   action :run
-  not_if { File.exists?('/etc/le/config') } 
+  not_if { File.exists?('/etc/le/config') }
 end
 
 follow_paths = [
   "/var/log/syslog",
   "/var/log/auth.log",
-  "/var/log/daemon.log"
+  "/var/log/daemon.log",
+  "/data/gcr2/shared/log/staging.log",
+  "/data/gcr2/shared/log/production.log"
 ]
 (node[:applications] || []).each do |app_name, app_info|
   follow_paths << "/var/log/nginx/#{app_name}.access.log"
@@ -21,7 +23,7 @@ end
 follow_paths.each do |path|
   execute "le follow #{path}" do
     command "le follow #{path}"
-    ignore_failure true 
+    ignore_failure true
     action :run
   end
 end
